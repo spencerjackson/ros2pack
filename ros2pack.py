908ec29b1a7107161d7f4690d3a0f181225b84cc
Spencer Jackson <spencerandrewjackson@yahoo.com>
2012-12-28 00:13:08 -0500

    Render RPM specs

--- ros2pack.py
+++ ros2pack.py
@@ -1,8 +1,9 @@
 import subprocess
 import sys
+import re
 import xml.etree.ElementTree as etree
 
-PACKAGE_PREFIX = "ros-"
+PACKAGE_PREFIX = "ros-{0}"
 
 class DependencyStateStore:
   def __init__(self, buildtool_depends, build_depends, run_depends):
@@ -11,12 +12,17 @@ class DependencyStateStore:
     self.marked_build = set()
     self.marked_run = set()
 
-  def __str__(self):
-    packages = self.unmarked_build.union(self.unmarked_run)
-    packages = packages.union(map(lambda pkg: PACKAGE_PREFIX + pkg,
-                                  self.marked_build.union(self.marked_run)))
+  def _prefix_marked_packages(self, packages):
+    return set(map(lambda pkg: PACKAGE_PREFIX.format(pkg),
+                   packages))
 
-    return packages.__str__()
+  def build_packages(self):
+    return self.unmarked_build.union(self._prefix_marked_packages(self.marked_build))
+  def run_packages(self):
+    return self.unmarked_run.union(self._prefix_marked_packages(self.marked_run))
+
+  def __str__(self):
+    return self.build_packages().union(self.run_packages()).__str__()
 
   def mark(self, package_name):
     if package_name in self.unmarked_build:
@@ -26,27 +32,74 @@ class DependencyStateStore:
       self.unmarked_run.discard(package_name)
       self.marked_run.add(package_name)
 
-def parsePackage(xmlPath):
-  tree = etree.parse(xmlPath)
-  root = tree.getroot()
-  def elementText(element):
-    return element.text
-  return DependencyStateStore(set(map(elementText,
-                                  root.findall('buildtool_depend'))),
-                              set(map(elementText,
-                                  root.findall('build_depend'))),
-                              set(map(elementText,
-                                  root.findall('run_depend'))))
+class RPMSpec:
+  def __init__(self, xmlPath, wsPath):
+    tree = etree.parse(xmlPath)
+    root = tree.getroot()
+    self.name = PACKAGE_PREFIX.format(root.find('name').text)
+    self.version = root.find('version').text
+    self.url = root.find('url').text
+    self.description = re.sub('\s+', ' ', root.find('description').text)
+    self.summary = self.description.split(".", 1)[0]
+    self.license = root.find('license').text
+    def elementText(element):
+      return element.text
+    self.dependencies = DependencyStateStore(set(map(elementText,
+                                                     root.findall('buildtool_depend'))),
+                                             set(map(elementText,
+                                                     root.findall('build_depend'))),
+                                             set(map(elementText,
+                                                     root.findall('run_depend'))))
+    with subprocess.Popen(["wstool", "info", "-t", wsPath, "--only", "localname"], stdout=subprocess.PIPE, universal_newlines=True) as provided_results:
+      for provided_result in provided_results.stdout:
+        provided = provided_result.rstrip()
+        self.dependencies.mark(provided)
 
-def identifyProvided(wsPath, dependencyStore):
-  with subprocess.Popen(["wstool", "info", "-t", wsPath, "--only", "localname"], stdout=subprocess.PIPE, universal_newlines=True) as provided_results:
-    for provided_result in provided_results.stdout:
-      provided = provided_result.rstrip()
-      dependencyStore.mark(provided)
-  return dependencyStore
+  def render(self, stream):
+    header_template = """
+Name:	{name}
+Version:	{version}
+Release:	0
+License:	{license}
+Summary:	{summary}
+Url:	{url}
+Group:	Robotics
+Source:	PLUG THIS IN
+
+"""
+    stream.write(header_template.format(name=self.name, version=self.version,
+                                        license=self.license, summary=self.summary,
+                                        url=self.url))
+
+    for build_dependency in self.dependencies.build_packages():
+      stream.write("BuildRequires:	{0}\n".format(build_dependency))
+    for run_dependency in self.dependencies.run_packages():
+      stream.write("Requires:	{0}\n".format(run_dependency))
+    stream.write("\n%description\n{0}".format(self.description))
+
+    body = """
+%prep
+%setup -q
+
+%build
+catkin_make --source . -DSETUPTOOLS_ARG_EXTRA=""
+
+%install
+catkin_make -DCMAKE_INSTALL_PREFIX=%{buildroot}/usr install
+
+%post
+
+%postun
+
+%files -f build/install_manifest.txt
+%defattr(-,root,root)
+
+%changelog
+"""
+    stream.write(body)
 
 if __name__ == '__main__':
   xmlPath = sys.argv[1]
   wsPath = sys.argv[2]
-  dependencies = parsePackage(xmlPath)
-  print(identifyProvided(wsPath, dependencies))
+  spec = RPMSpec(xmlPath, wsPath)
+  spec.render(sys.stdout)
