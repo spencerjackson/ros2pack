5f333a7b08cfb47f020dba603a5b4ccbe3eedf37
Spencer Jackson <spencerandrewjackson@yahoo.com>
2014-01-23 13:05:48 -0500

    Use catkin_pkg
    Use the catkin_pkg library to find and parse catkin packages. To
    be compatible, backport to python 2.7.

--- ros2spec.py
+++ ros2spec.py
@@ -6,13 +6,13 @@ import re
 import os.path
 import xml.etree.ElementTree as etree
 import argparse
-import urllib.request
+import urllib2
+
+from catkin_pkg.packages import find_packages
 
 import pdb
 # Encapsulates a list of dependencies
 class DependencyStore:
-
-  _cache = {}
 
   class Dependency:
     def __init__(self, name):
@@ -24,34 +24,34 @@ class DependencyStore:
 
     def resolve(self):
       if not subprocess.call(
-        ['rospack','find',self._name], stdout = subprocess.DEVNULL, 
-        stderr = subprocess.DEVNULL
-      ):
+        ['rospack','find',self._name]):
         self._resolved_name = self._name
         return
 
-      with subprocess.Popen(
-        ['rosdep', '--os=opensuse:13.1', 'resolve', self._name], stdout = subprocess.PIPE, 
-        stderr = subprocess.DEVNULL, universal_newlines=True
-      ) as rosdep_stream:
-        rosdep_result = rosdep_stream.stdout.readlines()
-        if len(rosdep_result) == 2:
-          self._resolved_name = rosdep_result[1]
-        else:
-          print(
+      rosdep_resolve = subprocess.Popen(
+        ['rosdep', '--os=opensuse:13.1', 'resolve', self._name], stdout = subprocess.PIPE,
+        universal_newlines=True, close_fds = True
+      )
+      rosdep_resolve.wait()
+      rosdep_result = rosdep_resolve.stdout.readlines()
+      if len(rosdep_result) == 2:
+        self._resolved_name = rosdep_result[1]
+      else:
+        print(
 """The dependency {name} could not be found by either both rospack or rosdep.
 Maybe you forgot to source the appropriate setup.bash, or there is no rosdep
 binding for {name} for this OS?""".format(name=self._name))
-          exit(1)
+        exit(1)
 
-  def get_dependency(name):
-    if name not in DependencyStore._cache:
-      DependencyStore._cache[name] = DependencyStore.Dependency(name) 
-    return DependencyStore._cache[name]
+  def get_dependency(self, name):
+    if name not in self._cache:
+      self._cache[name] = self.Dependency(name)
+    return self._cache[name]
 
   def __init__(self, buildtool_depends, build_depends, run_depends):
-    self._build = {p:DependencyStore.get_dependency(p) for p in build_depends + buildtool_depends + ['catkin', 'gtest']}
-    self._run = {p:DependencyStore.get_dependency(p) for p in run_depends}
+    self._cache = {}
+    self._build = {p: self.get_dependency(p) for p in build_depends + buildtool_depends + ['catkin', 'gtest']}
+    self._run = {p: self.get_dependency(p) for p in run_depends}
 
   def __str__(self):
     return "Build: {b}\nRun: {r}".format(b = self._build.__str__(), 
@@ -72,74 +72,61 @@ def extract_all_text(element):
     mod_lst.append(text)
   return re.sub('\s+', ' ', "".join(mod_lst).strip())
 
-class RPMSpec:
+class RPMSpec(object):
 
-  def factory(packagePath, wsPath, override, distro):
-    tree = etree.parse(packagePath+"/package.xml")
-    root = tree.getroot()
-    name = root.find('name').text
-    version = root.find('version').text
-    url = root.find('url').text
+  def __init__(self, package, wsPath, override, distro):
+    self.name = package['name']
+    self.version = package['version']
+    url_objects = package['urls']
+    for url_object in url_objects:
+      if url_object.type == "website":
+        self.url = url_object
+    else:
+      self.url = "http://www.ros.org"
+    self.license = package['licenses'][0]
 
     if override.description != None:
-      description = override.description
+      self.description = override.description
     else:
-      description = extract_all_text(root.find('description'))
-      description = description[0].upper() + description[1:]
-    
+      self.description = package['description']
+
     if override.summary != None:
-      summary = override.summary
+      self.summary = override.summary
     else:
-      summary = description.split(".", 1)[0]
-    
-    license = root.find('license').text
-    with subprocess.Popen(
-      ['wstool', 'info', '-t', wsPath, '--only', 'cur_uri,version', name], 
-      stdout = subprocess.PIPE, universal_newlines = True
-    ) as wstool:
-      str_out = re.sub('\n', '', wstool.stdout.readline())
-      if "ros-gbp" in str_out:
-        source = re.sub('\.git,', '/archive/', str_out) + '.tar.gz'
-        print("ros-gbp package detected. URL: " + source)
-      else:
-        source = re.sub(',.*', '', str_out)
+      self.summary = self.description.split(".", 1)[0]
 
-    def elementText(element):
-      return element.text
-
-    dependencies = DependencyStore(
-      list(map(elementText, root.findall('buildtool_depend'))),
-      list(map(elementText, root.findall('build_depend'))),
-      list(map(elementText, root.findall('run_depend')))
-    )
-
-    has_python = os.path.isfile(packagePath + "/setup.py")
-
-    if root.find("export") == None:
-      is_metapackage = False
+    wstool_info =  subprocess.Popen(
+      ['wstool', 'info', '-t', wsPath, '--only', 'cur_uri,version', self.name],
+      stdout = subprocess.PIPE, universal_newlines = True, close_fds = True)
+    wstool_info.wait()
+    str_out = re.sub('\n', '', wstool_info.stdout.readline())
+    self.make_tarball = False
+    if "ros-gbp" in str_out:
+      self.source = re.sub('\.git,', '/archive/', str_out) + '.tar.gz'
+      print("ros-gbp package detected. URL: " + self.source)
+    elif "" == str_out:
+      # We couldn't find *any* information about the package... Maybe it's custom?
+      # Bundle the source
+      self.source = package.name + "-" + package.version + ".tar.xv"
+      self.make_tarball = True
     else:
-      is_metapackage = root.find("export").find("metapackage") != None
+      self.source = re.sub(',.*', '', str_out)
 
-    return RPMSpec(name, version, source, url, description, summary, 
-                   license, dependencies, has_python, is_metapackage, distro)
+    def convert_to_names(packages):
+      return map(lambda package: package.name, packages)
 
-  def __init__(self, name, version, source, url, description, summary, license, 
-               dependencies, has_python, is_metapackage, distro):
-    self.name = name
-    self.version = version
-    self.source = source
-    self.url = url
-    #self.patches = patches
-    self.description = description
-    self.summary = summary
-    self.license = license
-    self.dependencies = dependencies
-    self.has_python = has_python
-    self.is_metapackage = is_metapackage
+    self.dependencies = DependencyStore(convert_to_names(package['buildtool_depends']),
+                                        convert_to_names(package['build_depends']),
+                                        convert_to_names(package['run_depends']))
+
+    self.has_python = os.path.isfile(os.path.join(os.path.dirname(package['filename']),
+                                                                  "setup.py"))
+
+    self.is_metapackage = package.is_metapackage()
+
     self.distro = distro
 
   def generate_service(self, stream):
-    
     download_files_srv = """  <service name="download_files"/>"""
     tar_scm_srv = """  <service name="tar_scm">
     <param name="url">{source}</param>
@@ -285,19 +272,29 @@ if __name__ == '__main__':
     overrides[package.attrib['name']] = generate_override(package)
 
   srcdir = args.workspace + '/src/'
-  if args.packages == None:
-    packages = [name for name in os.listdir(srcdir) if os.path.isdir(srcdir + name)]
-  else:
-    packages = args.packages
+
+  packages = find_packages(srcdir)
+
+  if args.packages != None:
+    subset_packages = {}
+    for (package_path, package_contents) in packages.iteritems():
+      print(package_contents.name)
+      if package_contents.name in args.packages:
+        subset_packages[package_path] = package_contents
+    packages = subset_packages
+
+  os.chdir(args.destination)
 
   print("Listing packages on server ...")
-  with subprocess.Popen(
-    ["osc", "list", args.destination.split('/')[-1]], 
-    stdout = subprocess.PIPE, universal_newlines = True) as server_results:
-    remote_packages = [line.replace('\n', '') for line in server_results.stdout]
+  osc_list = subprocess.Popen(
+    ["osc", "list", args.destination.split('/')[-1]],
+    stdout = subprocess.PIPE, universal_newlines = True, close_fds = True)
+  osc_list.wait()
+  remote_packages = [line.replace('\n', '') for line in osc_list.stdout]
   skip = args.pack_resume != None
 
-  for package in packages:
+  print(args.packages)
+  for package_key, package in packages.iteritems():
     if skip and (package != args.pack_resume):
       continue
     else:
@@ -312,16 +309,15 @@ if __name__ == '__main__':
       override = PackageOverride()
     if override.ignore:
       continue
-    spec = RPMSpec.factory(srcdir + '/' + package, srcdir, override, args.distro)
-    target_dir = args.destination + '/' + package
-    os.chdir(args.destination)
-    if package not in remote_packages:
-      print("Package " + package + " was not found on server.")
+    spec = RPMSpec(package, srcdir, override, args.distro)
+    target_dir = os.path.join(args.destination, package.name)
+    if package.name not in remote_packages:
+      print("Package " + package.name + " was not found on server.")
       if (os.path.exists(target_dir)):
         print("""The package was not found on the server, but the directory was found locally.
 Please resolve this manually before continuing.""")
         exit(1)
-      print("Creating package " + package + " ...")
+      print("Creating package " + package.name + " ...")
       subprocess.call(['osc', 'mkpac', target_dir])
       os.chdir(target_dir)
     else:
@@ -335,19 +331,24 @@ Please resolve this manually before continuing.""")
         subprocess.call(['osc', 'up'])
 
     print('Generating files in ' + target_dir + ' ...')
-    with open(target_dir + '/_service', mode = "w") as srv_file:
-      spec.generate_service(srv_file)
-    with open(target_dir + '/' + spec.name + ".spec", mode = "w") as rpmSpec:
+    if not spec.make_tarball:
+      with open(target_dir + '/_service', mode = "w+") as srv_file:
+        spec.generate_service(srv_file)
+    else:
+      subprocess.check_call(['tar', '--exclude-vcs', '-cvJf', os.path.join(target_dir, package.name + '-' + package.version + '.tar.xv'), os.path.dirname(package.filename)])
+    with open(target_dir + '/' + spec.name + ".spec", mode = "w+") as rpmSpec:
       spec.render(rpmSpec)
-    with open(target_dir + '/' + spec.name + "-rpmlintrc", mode = "w") as lintFile:
+    with open(target_dir + '/' + spec.name + "-rpmlintrc", mode = "w+") as lintFile:
       lintFile.write("""setBadness('devel-file-in-non-devel-package', 0)
 setBadness('shlib-policy-name-error', 0)""")
 
     subprocess.check_call(['osc', 'addremove'])
-    with subprocess.Popen(["osc", "st"], stdout = subprocess.PIPE) as status:
-      if status == '':
-        print("No changes to commit.")
-        continue
+    osc_st = subprocess.Popen(["osc", "st"], stdout = subprocess.PIPE, close_fds = True)
+    osc_st.wait()
+    status = osc_st.stdout.readlines()
+    if status == '':
+      print("No changes to commit.")
+      continue
     if (args.remote):
       print("Performing check-in...")
       subprocess.check_call(['osc', 'ci', '-m', '"ros2spec automated check-in"'])
